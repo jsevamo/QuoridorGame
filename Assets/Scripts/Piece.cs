0c8ffeb80cbb0fec995b75f9bee7a0cf4eaa18dc
Sebastián Vargas <j.sevamo@gmail.com>
2018-11-23 14:42:18 -0500

    Random Color of piece done

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
-public class Piece : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class Piece : MonoBehaviour
+{
+	[SerializeField]
+	private Color colorOfPiece;
 	
+	// Use this for initialization
+	void Start()
+	{
+		gameObject.GetComponent<Renderer>().material.color = 
+			new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f));
+		colorOfPiece = GetComponent<Renderer>().material.color;
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
 	}
 }
